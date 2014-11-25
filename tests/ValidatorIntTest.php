81398dc7897adeb4ff10909ca23ee213b49c2298
Florian CHAVELET <deathknight78@hotmail.fr>
2014-11-25 16:16:02 +0100

    Reglages / Tests  new

--- tests/ValidatorIntTest.php
+++ tests/ValidatorIntTest.php
@@ -6,25 +6,25 @@
  * Time: 10:40
  */
 
-namespace tests\Web1\Validator;
+namespace Tests\Web1\Validator;
 
 use Web1\Validator\ValidatorInt;
 
 class ValidatorIntTest extends \PHPUnit_Framework_TestCase
 {
 
-    /*public function testIntEqual()
+    public function testIntEqual()
     {
         $a = mt_rand(1, 100);
         $test = ValidatorInt::intEqual($a,$a);
         $this->assertTrue($test);
-    }*/
+    }
 
-    /*public function testIntInf()
+    public function testIntInf()
     {
         $a = mt_rand(1, 49);
         $b = mt_rand(50, 100);
         $test = ValidatorInt::intInf($a,$b);
         $this->assertTrue($test);
-    }*/
+    }
 } 
